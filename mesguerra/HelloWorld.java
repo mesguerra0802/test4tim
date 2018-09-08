21fff9a06c2b3f64421cea9013bf3b7e717b52c4
Mark Esguerra <mark@kibblebytes.com>
2018-09-08 15:03:35 -0700

    Adding necessary package identifiers to allow multiple mains in project

--- mesguerra/HelloWorld.java
+++ mesguerra/HelloWorld.java
@@ -1,4 +1,4 @@
-
+package mesguerra;
 public class HelloWorld {
 
 	public static void main(String[] args) {
--- tgoodman/HelloWorld.java
+++ tgoodman/HelloWorld.java
@@ -1,4 +1,4 @@
-
+package tgoodman;
 public class HelloWorld {
 
 	public static void main(String[] args) {
